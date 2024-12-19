102db16deed1b898f259c322e85bacb5c295dcdb
Goncharov Dmitriy <gdmitry58@gmail.com>
2024-12-19 18:36:06 +0300

    Метод расчета количества материала для производства

---
+++ ProductionCalculator/ProductionCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionCalculator
+{
+    public static class ProductionCalculator
+    {
+        public static int CalculateMaterial(
+            int productTypeCoefficient,
+            int materialDefectRate,
+            int productQuantity,
+            double parameter1,
+            double parameter2)
+        {
+
+            if (productTypeCoefficient <= 0 || productQuantity <= 0 ||
+                parameter1 <= 0 || parameter2 <= 0 || materialDefectRate <= 0)
+            {
+                return -1;
+            }
+
+            double materialPerUnit = parameter1 * parameter2 * productQuantity;
+            double totalMaterial = materialPerUnit * productQuantity;
+            double defectMultiplier = 1 + (materialDefectRate / 100);
+
+            int result = (int)Math.Ceiling(totalMaterial * defectMultiplier);
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+}
